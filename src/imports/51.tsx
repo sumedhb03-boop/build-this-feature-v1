15ad2b2d6848997b37a1a7b80b925271c3ff9a07
Sumedh Bambal <sumedhb03@Sumedhs-MacBook-Air.local>
2026-02-22 10:38:41 +0530

    v1

---
+++ src/imports/51.tsx
@@ -0,0 +1,458 @@
+import imgFreeHandHoldingIPhone16ProMockup2 from "figma:asset/cb404150f92e88ed4a66f5525a525988047fb537.png";
+import imgLucrenteHomepage1 from "figma:asset/934e76980decd08a547dbf8807403b703fd07584.png";
+import imgShotsMockups21 from "figma:asset/f0cf51fe902c49e33273901f4d5947d40e8551e8.png";
+import imgShotsMockups131 from "figma:asset/8b29ebd34c3524dbc7989b4884ebd904c19ff803.png";
+
+function Group1() {
+  return (
+    <div className="-translate-x-1/2 absolute h-[515px] left-[calc(50%-0.5px)] top-[337px] w-[31px]">
+      <div className="absolute inset-[-0.19%_0_0_0]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 31 516">
+          <g id="Group 4353" opacity="0.2">
+            <line id="Line 257" stroke="var(--stroke-0, white)" x1="16.5" x2="16.5" y1="1" y2="516" />
+            <line id="Line 258" stroke="var(--stroke-0, white)" x2="31" y1="0.5" y2="0.5" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Frame18() {
+  return (
+    <div className="-translate-x-1/2 absolute bg-black h-[357px] left-1/2 top-[426px] w-[746px]">
+      <div className="absolute font-['Helvetica_Neue_LT_Std:77_Bold_Condensed',sans-serif] leading-[0] left-0 not-italic right-0 text-[0px] text-[80px] text-center text-white top-[calc(50%-143.5px)] tracking-[-3.2px] uppercase whitespace-pre-wrap">
+        <p className="leading-[0.9] mb-0">{`Design `}</p>
+        <p className="mb-0">
+          <span className="leading-[0.9] text-[#ff4c11]">Partner</span>
+          <span className="leading-[0.9]">{` for `}</span>
+        </p>
+        <p className="leading-[0.9] mb-0">{`Early-Stage `}</p>
+        <p>
+          <span className="leading-[0.9]">{`Startups & `}</span>
+          <span className="leading-[0.9] text-white">Teams</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Frame21() {
+  return (
+    <div className="content-stretch flex gap-[6px] items-end relative shrink-0">
+      <p className="font-['Geist:Regular',sans-serif] font-normal leading-[1.2] opacity-80 relative shrink-0 text-[#fbf9ef] text-[13px] tracking-[-0.13px]">Hello, I’m Sumedh</p>
+      <div className="relative shrink-0 size-[20px]">
+        <svg className="absolute block inset-0" fill="none" preserveAspectRatio="none" viewBox="0 0 20 20">
+          <circle cx="10" cy="10" fill="var(--fill-0, #2C217D)" id="Ellipse 652" r="10" />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Frame22() {
+  return (
+    <div className="content-stretch flex flex-col gap-[3px] items-start relative shrink-0 w-[266px]">
+      <Frame21 />
+      <p className="font-['Geist:Regular',sans-serif] font-normal leading-[1.2] min-w-full opacity-60 relative shrink-0 text-[13px] text-white tracking-[-0.13px] w-[min-content] whitespace-pre-wrap">Product designer, currently working freelance.</p>
+    </div>
+  );
+}
+
+function Frame23() {
+  return (
+    <div className="content-stretch flex font-normal gap-[8px] items-center leading-[0.98] relative shrink-0 text-[12px] text-white">
+      <p className="font-['Geist_Mono:Regular',sans-serif] relative shrink-0 uppercase">About</p>
+      <p className="font-['Geist:Regular',sans-serif] relative shrink-0"> //</p>
+      <p className="font-['Geist_Mono:Regular',sans-serif] relative shrink-0 uppercase">Contact</p>
+    </div>
+  );
+  //</p><p className="font-['Geist_Mono:Regular',sans-serif] relative shrink-0 uppercase">Contact</p></div>);
+  //</p><p className="font-['Geist_Mono:Regular',sans-serif] relative shrink-0 uppercase">Contact</p></div>);
+}
+
+function Frame() {
+  return (
+    <div className="absolute content-stretch flex items-start justify-between left-0 pt-[24px] px-[32px] right-0 top-0">
+      <Frame22 />
+      <Frame23 />
+    </div>
+  );
+}
+
+function Frame8() {
+  return (
+    <div className="relative shrink-0 size-[26px]">
+      <svg className="absolute block inset-0" fill="none" preserveAspectRatio="none" viewBox="0 0 26 26">
+        <g clipPath="url(#clip0_1_104)" id="Frame 2085663155">
+          <circle cx="13" cy="13" fill="var(--fill-0, #FF4C11)" id="Ellipse 650" opacity="0.2" r="13" />
+          <circle cx="13" cy="13" fill="var(--fill-0, #FF4C11)" id="Ellipse 649" opacity="0.4" r="8.125" />
+          <circle cx="13" cy="13" fill="var(--fill-0, #FF4C11)" id="Ellipse 648" r="3.25" />
+        </g>
+        <defs>
+          <clipPath id="clip0_1_104">
+            <rect fill="white" height="26" width="26" />
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function Frame1() {
+  return (
+    <div className="content-stretch flex items-center relative shrink-0">
+      <p className="font-['Geist:Medium',sans-serif] font-medium leading-none relative shrink-0 text-[#fbf9ef] text-[12px] tracking-[-0.72px]">Open to</p>
+    </div>
+  );
+}
+
+function Frame6() {
+  return (
+    <div className="content-stretch flex items-center relative shrink-0">
+      <Frame1 />
+    </div>
+  );
+}
+
+function Frame4() {
+  return (
+    <div className="content-stretch flex flex-col gap-[2px] items-start relative shrink-0">
+      <Frame6 />
+      <p className="font-['Geist:Medium',sans-serif] font-medium leading-none opacity-40 relative shrink-0 text-[#fbf9ef] text-[12px] tracking-[-0.36px]">full-time roles</p>
+    </div>
+  );
+}
+
+function Frame7() {
+  return (
+    <div className="content-stretch flex gap-[8px] items-center relative shrink-0">
+      <Frame8 />
+      <Frame4 />
+    </div>
+  );
+}
+
+function Frame3() {
+  return (
+    <div className="content-stretch flex gap-[2px] items-center relative shrink-0 tracking-[-0.72px]">
+      <p className="relative shrink-0">10:24</p>
+      <p className="opacity-40 relative shrink-0">PM</p>
+    </div>
+  );
+}
+
+function Frame2() {
+  return (
+    <div className="content-stretch flex flex-col font-['Geist:Medium',sans-serif] font-medium gap-[2px] items-end justify-end leading-none relative shrink-0 text-[#fbf9ef] text-[12px] w-[107px]">
+      <Frame3 />
+      <p className="opacity-40 relative shrink-0 tracking-[-0.36px]">Mumbai, India</p>
+    </div>
+  );
+}
+
+function Frame5() {
+  return (
+    <div className="absolute content-stretch flex items-center justify-between left-0 pb-[24px] px-[32px] right-0 top-[802px]">
+      <Frame7 />
+      <Frame2 />
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="-translate-x-1/2 absolute h-[140px] left-[calc(50%-0.5px)] top-[-75px] w-[31px]">
+      <div className="absolute inset-[0_0_-0.71%_0]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 31 141">
+          <g id="Group 4352" opacity="0.2">
+            <line id="Line 259" stroke="var(--stroke-0, white)" x1="15.5" x2="15.5" y1="140" y2="2.42842e-08" />
+            <line id="Line 260" stroke="var(--stroke-0, white)" x1="31" x2="4.33992e-08" y1="140.5" y2="140.5" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Frame9() {
+  return (
+    <div className="-translate-x-1/2 absolute bg-[#121212] h-[109px] left-[calc(50%-367px)] overflow-clip top-[262px] w-[180px]">
+      <div className="-translate-x-1/2 absolute h-[109px] left-1/2 top-0 w-[180px]" data-name="Free_Hand_Holding_iPhone_16_Pro_Mockup 2">
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+          <img alt="" className="absolute h-[123.92%] left-[-0.04%] max-w-none top-[-6.46%] w-[100.41%]" src={imgFreeHandHoldingIPhone16ProMockup2} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame11() {
+  return (
+    <div className="-translate-x-1/2 absolute bg-[#121212] h-[109px] left-[calc(50%+368px)] overflow-clip top-[262px] w-[180px]">
+      <div className="-translate-x-1/2 -translate-y-1/2 absolute h-[120px] left-1/2 top-[calc(50%-5.5px)] w-[180px]" data-name="Lucrente Homepage 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgLucrenteHomepage1} />
+      </div>
+    </div>
+  );
+}
+
+function Frame10() {
+  return (
+    <div className="-translate-x-1/2 absolute bg-[#121212] h-[81.75px] left-[calc(50%-566.5px)] overflow-clip top-[404px] w-[135px]">
+      <div className="absolute h-[90px] left-0 top-[-3.75px] w-[135px]" data-name="Shots Mockups (2) 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgShotsMockups21} />
+      </div>
+    </div>
+  );
+}
+
+function Frame20() {
+  return (
+    <div className="absolute content-stretch flex flex-col items-end leading-[0.98] left-[36px] pb-[1.5px] text-white top-[43.5px] w-[93px] whitespace-pre-wrap">
+      <p className="font-['Times_New_Roman:Italic',sans-serif] italic relative shrink-0 text-[15px] text-right tracking-[-0.45px] w-full">My</p>
+      <p className="font-['Helvetica_Neue:Bold',sans-serif] not-italic opacity-40 relative shrink-0 text-[18px] tracking-[-0.54px] w-full">Playground</p>
+    </div>
+  );
+}
+
+function Frame12() {
+  return (
+    <div className="-translate-x-1/2 absolute bg-[#ff4c11] h-[81.75px] left-[calc(50%+566.5px)] overflow-clip top-[404px] w-[135px]">
+      <Frame20 />
+    </div>
+  );
+}
+
+function Frame27() {
+  return (
+    <div className="content-stretch flex items-start justify-end pt-[48px] relative shrink-0 w-[44px]">
+      <p className="font-['Geist_Mono:Regular',sans-serif] font-normal leading-[0.98] opacity-30 relative shrink-0 text-[12px] text-right text-white">02</p>
+    </div>
+  );
+}
+
+function Frame15() {
+  return (
+    <div className="h-[17px] opacity-20 relative w-full">
+      <div className="absolute flex h-0 items-center justify-center left-0 top-0 w-[17px]">
+        <div className="flex-none rotate-180">
+          <div className="h-0 relative w-[17px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17 1">
+                <line id="Line 261" stroke="var(--stroke-0, white)" x2="17" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute flex h-[17px] items-center justify-center left-px top-0 w-0" style={{ "--transform-inner-width": "1200", "--transform-inner-height": "153.5" } as React.CSSProperties}>
+        <div className="-rotate-90 flex-none">
+          <div className="h-0 relative w-[17px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17 1">
+                <line id="Line 262" stroke="var(--stroke-0, white)" x2="17" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame14() {
+  return (
+    <div className="h-[17px] opacity-20 relative w-full">
+      <div className="absolute flex h-0 items-center justify-center left-0 top-0 w-[17px]">
+        <div className="flex-none rotate-180">
+          <div className="h-0 relative w-[17px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17 1">
+                <line id="Line 261" stroke="var(--stroke-0, white)" x2="17" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute flex h-[17px] items-center justify-center left-px top-0 w-0" style={{ "--transform-inner-width": "1200", "--transform-inner-height": "153.5" } as React.CSSProperties}>
+        <div className="-rotate-90 flex-none">
+          <div className="h-0 relative w-[17px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17 1">
+                <line id="Line 262" stroke="var(--stroke-0, white)" x2="17" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame24() {
+  return (
+    <div className="content-stretch flex flex-col gap-[214px] items-start mr-[-3px] relative shrink-0 w-[17px]">
+      <div className="flex h-[17px] items-center justify-center relative shrink-0 w-full" style={{ "--transform-inner-width": "1200", "--transform-inner-height": "307" } as React.CSSProperties}>
+        <div className="-scale-y-100 flex-none rotate-90 w-full">
+          <Frame15 />
+        </div>
+      </div>
+      <div className="flex h-[17px] items-center justify-center relative shrink-0 w-full" style={{ "--transform-inner-width": "1200", "--transform-inner-height": "307" } as React.CSSProperties}>
+        <div className="-rotate-90 flex-none w-full">
+          <Frame14 />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame13() {
+  return (
+    <div className="bg-[#121212] h-[220px] mr-[-3px] overflow-clip relative shrink-0 w-[400px]">
+      <div className="-translate-x-1/2 -translate-y-1/2 absolute h-[502px] left-[calc(50%-0.5px)] top-[calc(50%+122px)] w-[401px]" data-name="Shots Mockups (13) 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgShotsMockups131} />
+      </div>
+      <div className="-translate-x-1/2 absolute h-[221px] left-[calc(50%-112px)] top-[-18px] w-[294px]" data-name="Free_Hand_Holding_iPhone_16_Pro_Mockup 1" />
+    </div>
+  );
+}
+
+function Frame17() {
+  return (
+    <div className="h-[17px] opacity-20 relative w-full">
+      <div className="absolute flex h-0 items-center justify-center left-0 top-0 w-[17px]">
+        <div className="flex-none rotate-180">
+          <div className="h-0 relative w-[17px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17 1">
+                <line id="Line 261" stroke="var(--stroke-0, white)" x2="17" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute flex h-[17px] items-center justify-center left-px top-0 w-0" style={{ "--transform-inner-width": "1200", "--transform-inner-height": "153.5" } as React.CSSProperties}>
+        <div className="-rotate-90 flex-none">
+          <div className="h-0 relative w-[17px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17 1">
+                <line id="Line 262" stroke="var(--stroke-0, white)" x2="17" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame16() {
+  return (
+    <div className="h-[17px] opacity-20 relative w-full">
+      <div className="absolute flex h-0 items-center justify-center left-0 top-0 w-[17px]">
+        <div className="flex-none rotate-180">
+          <div className="h-0 relative w-[17px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17 1">
+                <line id="Line 261" stroke="var(--stroke-0, white)" x2="17" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute flex h-[17px] items-center justify-center left-px top-0 w-0" style={{ "--transform-inner-width": "1200", "--transform-inner-height": "153.5" } as React.CSSProperties}>
+        <div className="-rotate-90 flex-none">
+          <div className="h-0 relative w-[17px]">
+            <div className="absolute inset-[-1px_0_0_0]">
+              <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 17 1">
+                <line id="Line 262" stroke="var(--stroke-0, white)" x2="17" y1="0.5" y2="0.5" />
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame25() {
+  return (
+    <div className="content-stretch flex flex-col gap-[214px] items-start mr-[-3px] relative shrink-0 w-[17px]">
+      <div className="flex h-[17px] items-center justify-center relative shrink-0 w-full" style={{ "--transform-inner-width": "1200", "--transform-inner-height": "307" } as React.CSSProperties}>
+        <div className="flex-none rotate-90 w-full">
+          <Frame17 />
+        </div>
+      </div>
+      <div className="flex h-[17px] items-center justify-center relative shrink-0 w-full" style={{ "--transform-inner-width": "1200", "--transform-inner-height": "307" } as React.CSSProperties}>
+        <div className="-rotate-90 -scale-y-100 flex-none w-full">
+          <Frame16 />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Frame26() {
+  return (
+    <div className="content-stretch flex items-center pr-[3px] relative shrink-0 w-[428px]">
+      <Frame24 />
+      <Frame13 />
+      <Frame25 />
+    </div>
+  );
+}
+
+function Frame19() {
+  return (
+    <div className="content-stretch flex flex-col font-['Geist:Regular',sans-serif] font-normal gap-[4px] items-start leading-[0.98] opacity-40 pt-[48px] relative shrink-0 text-[12px] text-white w-[44px] whitespace-pre-wrap">
+      <p className="relative shrink-0 w-full">Brand</p>
+      <p className="relative shrink-0 w-full">Product</p>
+    </div>
+  );
+}
+
+function Frame28() {
+  return (
+    <div className="content-stretch flex items-start relative shrink-0 w-full">
+      <Frame27 />
+      <Frame26 />
+      <Frame19 />
+    </div>
+  );
+}
+
+function Frame29() {
+  return (
+    <div className="absolute content-stretch flex flex-col items-center left-[375px] top-0 w-[516px]">
+      <Frame28 />
+      <p className="font-['Geist_Mono:Regular',sans-serif] font-normal leading-[0.98] relative shrink-0 text-[12px] text-center text-white w-full whitespace-pre-wrap">SCORECRIC</p>
+    </div>
+  );
+}
+
+function Frame30() {
+  return (
+    <div className="-translate-x-1/2 absolute h-[485.75px] left-1/2 top-[65px] w-[1268px]">
+      <Frame9 />
+      <Frame11 />
+      <Frame10 />
+      <Frame12 />
+      <Frame29 />
+    </div>
+  );
+}
+
+export default function Component() {
+  return (
+    <div className="bg-black relative size-full" data-name="51">
+      <Group1 />
+      <Frame18 />
+      <Frame />
+      <Frame5 />
+      <Group />
+      <Frame30 />
+      <p className="absolute font-['Geist:Regular',sans-serif] font-normal leading-[1.2] left-[32px] opacity-40 text-[#fbf9ef] text-[13px] top-[115px] tracking-[-0.13px] w-[183px] whitespace-pre-wrap">{`This is my corner of the internet where I showcase my work and variety of other things I'm currently exploring.`}</p>
+    </div>
+  );
+}
